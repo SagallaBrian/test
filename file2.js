99964c6d59d8484e193f1a0f282922eca3ed1c81
SagallaBrian <brayozwebdesigner@gmail.com>
2021-01-05 23:14:32 +0300

    Branch 1v2 commit 1

--- file2.js
+++ file2.js
@@ -6,3 +6,6 @@ console.log("This is the End of Commit 2 File 2");
 
 console.log("This is the End of branch Commit 1 File 2");
 // end of branch commit 1 file 2
+
+console.log("This is the End of branch 1v2 Commit 1 File 2");
+// end of branch 1v2 commit 1 file 2
--- fold1/fold1.js
+++ fold1/fold1.js
@@ -6,3 +6,6 @@ console.log("This is the End of Commit 2 Fold 1");
 
 console.log("This is the End of branch Commit 1 Fold 1");
 // end of branch commit 1 fold 1
+
+console.log("This is the End of branch 1v2 Commit 1 Fold 1");
+// end of branch 1v2 commit 1 fold 1

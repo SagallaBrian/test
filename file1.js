3414b920ac6814b4c15eb54aa77218dd725ebf49
SagallaBrian <brayozwebdesigner@gmail.com>
2021-01-05 23:16:20 +0300

    Branch 2v2 commit 1

--- file1.js
+++ file1.js
@@ -6,3 +6,6 @@ console.log("This is the End of Commit 2 File 1");
 
 console.log("This is the End of branch Commit 1 File 1");
 // end of branch commit 1 file 1
+
+console.log("This is the End of branch 2v2 Commit 1 File 1");
+// end of branch 2v2 commit 1 file 1
